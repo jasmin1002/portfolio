723f15cfc1ac75fbfd81be0b8665ec910c10b995
Jasmin1002 <jasmineolofin@gmail.com>
2022-08-13 06:38:15 +0100

    Updated changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import Card from './Card'
 import Graph from './Graph'
+import {Account} from './Account'
 
-import {MdMoreHoriz} from 'react-icons/md'
 import {FcLike, FcConferenceCall,} from 'react-icons/fc'
 import {FaCaretUp, FaCaretDown, FaRegBell} from 'react-icons/fa'
 import {MdFollowTheSigns} from 'react-icons/md'
-
-import user from '../img/user.jpg'
 
 export default function Main() {
     return (
@@ -44,57 +42,7 @@ export default function Main() {
                 </section>
             </main>
             <aside className='content-aside'>
-                <section className='account-info'>
-                    <div className='account-top'>
-                        <h4 className='secondary-heading'>
-                            Account
-                        </h4>
-                        <a href='.' className='account-moreinfo'>
-                            <MdMoreHoriz className='icon-horizdot'/>
-                        </a>
-                    </div>
-                    <div className='account-body'>
-                        <div className='account-profile'>
-                            <figure className='account-avatar'>
-                                <img src={user} alt='Jasmine' className='account-photo'/>
-                            </figure>
-                            <div className='account-description'>
-                                <p className='account-username'>
-                                    @jasminefiles
-                                </p>
-                                <span className='account-username-id'>
-                                    0we60F_AE40
-                                </span>
-                            </div>
-                        </div>
-                        <ul className='online-presence'>
-                            <li className='online-stat'>
-                                <span className='online-stat-figure'>
-                                    367
-                                </span>
-                                <span className='online-activity'>
-                                    Posts
-                                </span>
-                            </li>
-                            <li className='online-stat'>
-                                <span className='online-stat-figure'>
-                                    246,772
-                                </span>
-                                <span className='online-activity'>
-                                    Followers
-                                </span>
-                            </li>
-                            <li className='online-stat'>
-                                <span className='online-stat-figure'>
-                                    2,782
-                                </span>
-                                <span className='online-activity'>
-                                    Following
-                                </span>
-                            </li>
-                        </ul>
-                    </div>
-                </section>
+                <Account />
                 <section className='ads'>
                     &nbsp;
                 </section>
