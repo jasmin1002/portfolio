762d36c41160af03b89ca93e4eadb02b4be71bc1
Jasmin1002 <jasmineolofin@gmail.com>
2022-08-20 15:04:13 +0100

    Modification made on Main.js and dashboard.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,51 +1,57 @@
-import React from 'react'
-import Card from './Card'
-import Graph from './Graph'
-import {Account} from './Account'
-import {Ads} from './Ads'
+import React from "react"
+import Card from "./Card"
+import Graph from "./Graph"
+import Upload from "./Upload"
+import Earning from "./Earning"
+import { Account } from "./Account"
+import { Ads } from "./Ads";
 
-import {FcLike, FcConferenceCall,} from 'react-icons/fc'
-import {FaCaretUp, FaCaretDown, FaRegBell} from 'react-icons/fa'
-import {MdFollowTheSigns} from 'react-icons/md'
+import { FcLike, FcConferenceCall } from "react-icons/fc";
+import { FaCaretUp, FaCaretDown, FaRegBell } from "react-icons/fa";
+import { MdFollowTheSigns } from "react-icons/md";
 
 export default function Main() {
-    return (
-        <section className='content-page row-grid'>
-            <main className='content-main'>
-                <section className='cards row-grid mb-2'>
-                    <Card 
-                        icon={<FcConferenceCall className='icon-followers'/>}
-                        sIcon={<FaCaretUp className='icon-clue' />}
-                        color='hsl(55, 137%, 86%)'
-                        title='Followers'
-                        sbgc='rgba(179, 255, 204, 0.54)'
-                        sfc='rgb(0, 231, 22)'
-                    />
-                    <Card 
-                        icon={<FcLike className='icon-likes'/>}
-                        sIcon={<FaCaretDown className='icon-clue' />}
-                        color='hsl(345, 97%, 86%)'
-                        title='Likes'
-                        sbgc='rgb(254, 185, 202)'
-                        sfc='red'
-                    />
-                    <Card 
-                        icon={<MdFollowTheSigns className='icon-reach'/>}
-                        sIcon={<FaCaretUp className='icon-clue' />}
-                        color='hsl(207, 51%, 95%)'
-                        title='Reach'
-                        sbgc='rgba(179, 255, 204, 0.54)'
-                        sfc='rgb(0, 231, 22)'
-                    />
-                </section>
-                <section className='row-grid'>
-                    <Graph />
-                </section>
-            </main>
-            <aside className='content-aside'>
-                <Account />
-                <Ads />
-            </aside>
+  return (
+    <section className="content-page row-grid">
+      <main className="content-main">
+        <section className="cards row-grid mb-2">
+          <Card
+            icon={<FcConferenceCall className="icon-followers" />}
+            sIcon={<FaCaretUp className="icon-clue" />}
+            color="hsl(55, 137%, 86%)"
+            title="Followers"
+            sbgc="rgba(179, 255, 204, 0.54)"
+            sfc="rgb(0, 231, 22)"
+          />
+          <Card
+            icon={<FcLike className="icon-likes" />}
+            sIcon={<FaCaretDown className="icon-clue" />}
+            color="hsl(345, 97%, 86%)"
+            title="Likes"
+            sbgc="rgb(254, 185, 202)"
+            sfc="red"
+          />
+          <Card
+            icon={<MdFollowTheSigns className="icon-reach" />}
+            sIcon={<FaCaretUp className="icon-clue" />}
+            color="hsl(207, 51%, 95%)"
+            title="Reach"
+            sbgc="rgba(179, 255, 204, 0.54)"
+            sfc="rgb(0, 231, 22)"
+          />
         </section>
-    )
+        <section className="row-grid mb-2">
+          <Graph />
+        </section>
+        <section className="row-grid">
+          <Upload />
+          <Earning />
+        </section>
+      </main>
+      <aside className="content-aside">
+        <Account />
+        <Ads />
+      </aside>
+    </section>
+  );
 }
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Pane from '../components/Pane'
-import Content from '../components/Content'
+import React from "react";
+import Pane from "../components/Pane";
+import Content from "../components/Content";
 
-import '../Dashboard.css'
+import "../Dashboard.css";
 
 export default function Dashboard() {
-    return (
-        <section className='dashboard'>
-            <Pane />
-            <Content />
-        </section>
-    )
+  return (
+    <section className="dashboard">
+      <Pane />
+      <Content />
+    </section>
+  );
 }
