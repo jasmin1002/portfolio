75696239d1843ba1ea6516f066fe66ec461276a7
Jasmin1002 <jasmineolofin@gmail.com>
2022-08-20 13:34:29 +0100

    Modification added to login and App page style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import SharedLayout from './pages/sharedLayout';
-import Login from './pages/login'
-import Dashboard from './pages/dashboard';
-import Page404 from './pages/page404'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./utils/auth";
+import SharedLayout from "./pages/sharedLayout";
+import Signup from "./pages/signup"
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import Page404 from "./pages/page404";
 
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/'  element={<SharedLayout />}>
-          <Route index path='/' element={<Login />} />
-          <Route index path='login' element={<Navigate to={'/'} />} />
-          <Route path='dashboard' element={<Dashboard />}/>
-          <Route path='*' element={<Page404 />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index path="/" element={<Login />} />
+            <Route index path="login" element={<Navigate to={"/"} />} />
+            <Route path='signup' element={<Signup />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="*" element={<Page404 />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,56 +1,85 @@
-import {React, useState} from 'react'
-import '../Login.css'
+import { React, useState } from "react";
+import { authenticate, useAuthContext } from "../utils/auth";
+import { NavLink, useNavigate } from "react-router-dom";
+import {FaNapster} from 'react-icons/fa'
+import "../Login.css";
 
 export default function Login() {
-    const [user, setUser] = useState({
-        username: '',
-        email: ''
-    })
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-    function handleChange(e) {
-        const {name, value} = e.target
-        setUser(prevUser => ({
-            ...prevUser,
-            [name]: value
-        }))
+  const {logIn} = useAuthContext();
+  const navigate = useNavigate();
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const {isAuthenticated, member} = authenticate(user);
+    if (isAuthenticated) {
+      // Do something...
+      console.log("Yessss authenticated!")
+      logIn(member);
+      navigate('/dashboard');
     }
+  }
 
-    function handleSubmit(e) {
-        e.preventDefault();
-
-        console.log(user)
-    }
-
-    return (
-        <section className='login'>
-            <form className='form' onSubmit={handleSubmit}>
-                <h1 className='login-heading'>Login</h1>
-                <div className='form-vgroup'>
-                    <label htmlFor='username'>Username</label>
-                    <input type='text'
-                        className='input'
-                        id='username'
-                        name='username'
-                        value={user.username}
-                        onChange={handleChange}
-                        // placeholder='username'
-                    />
-                </div>
-                <div className='form-vgroup'>
-                    <label htmlFor='email'>Email</label>
-                    <input type='text'
-                        className='input'
-                        id='email'
-                        name='email'
-                        value={user.email}
-                        onChange={handleChange}
-                        // placeholder='email'
-                    />
-                </div>
-                <div className='form-vgroup'>
-                    <button>Login</button>
-                </div>
-            </form>
-        </section>
-    )
+  return (
+    <section className="backdrop">
+      <div className="login">
+        <aside>
+          <FaNapster className="icon-logo"/>
+          <h1>OnlinePresenz</h1>
+          <p>
+            Monetize your online presence.
+          </p>
+          <p className="signup">
+            New here? <NavLink to='signup'>Sign Up</NavLink>
+          </p>
+        </aside>
+        <form className="form" onSubmit={handleSubmit}>
+          <h1 className="login-heading">Login</h1>
+          <div className="form-vgroup">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              className="input"
+              id="email"
+              name="email"
+              value={user.email}
+              onChange={handleChange}
+              placeholder="johndoe@dev.com"
+            />
+          </div>
+          <div className="form-vgroup">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="input"
+              id="password"
+              name="password"
+              value={user.password}
+              onChange={handleChange}
+              placeholder="Enter your password"
+            />
+            <p className="forgetpassword">
+              <NavLink to=".">Forget Password?</NavLink>
+            </p>
+          </div>
+          <div className="form-vgroup">
+            <button>Login</button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 }
